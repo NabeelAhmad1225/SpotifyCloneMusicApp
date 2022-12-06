6522b91ba7663c7c5a7877dd80e6584acf86dc4a
Nabeel Ahmad <nab34923@gmail.com>
2022-12-06 17:36:03 +0500

    project Setup

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,24 @@
+import {
+  createApi,
+  fakeBaseQuery,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "8da4a6ac81msh69bb66b48c2a34cp1f66fcjsn12e7a71cd208"
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
